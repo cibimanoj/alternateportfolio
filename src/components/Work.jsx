4b3442858d3818441209ed5ffd741a78848f932b
cibimanoj <86293164+cibimanoj@users.noreply.github.com>
2022-05-07 11:10:53 +0530

    updated

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import amazon from "../assets/amazon.png";
+import jobify from "../assets/jobify.png";
 import map from "../assets/map.png";
 import memories from "../assets/memories.png";
 import videochat from "../assets/videochat.png";
@@ -18,6 +18,57 @@ const Work = () => {
         {/* Container */}
         {/* Grid Item */}
         <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${jobify})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+           >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <div className="pt-8 text-center">
+                <a
+                  href="https://jobifyprojectmanagement.herokuapp.com/welcome"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-600 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/cibimanoj/capstonetask.git"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-600 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
+                    Code
+                  </button>
+                  <p className="text-center text-black	color: rgb(0 0 0)">
+                    This is a MERN project management tool app ,In which we can
+                    add the task,delete the task and update the task.
+                  </p>
+                </a>
+                <h6 className="font-medium text-xl text-black">STACK</h6>
+                <div>
+                  <div class="flex gap-4 p-4 items-center text-lg text-zinc-50 ">
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      MongoDB{" "}
+                    </div>
+                    <div className="contents">
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        Express
+                      </div>
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        React
+                      </div>
+                    </div>
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      Node
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div
             style={{ backgroundImage: `url(${map})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -26,7 +77,7 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <div className="pt-9 text-center">
                 <a href="https://mappin.netlify.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50  bg-amber-500  font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50  bg-amber-600  font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
                     Demo
                   </button>
                 </a>
@@ -34,64 +85,37 @@ const Work = () => {
                   href="https://github.com/cibimanoj/mappin_frontend.git"
                   target="_blank"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-500 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-600 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
                     Code
                   </button>
                 </a>
-                <p className="text-center text-stone-50	color: rgb(250 250 249)">
-                  This is a MERN web application.In this app where we can pin our
-                  locations of our house,office and favourite places.
+                <p className="text-center text-black	color: rgb(0 0 0)">
+                  This is a MERN web application.In this app where we can pin
+                  our locations of our house,office and favourite places.
                 </p>
                 <h6 className="font-medium text-xl text-black  ">STACK</h6>
                 <div>
                   <div class="flex gap-4 p-4 items-center text-lg text-zinc-50 ">
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">MongoDB </div>
-                    <div className="contents">
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500"  >Express</div>
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">React</div>
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      MongoDB{" "}
                     </div>
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">Node</div>
+                    <div className="contents">
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        Express
+                      </div>
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        React
+                      </div>
+                    </div>
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      Node
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
 
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${amazon})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <div className="pt-8 text-center">
-                <a href="https://cibiamazonclone.netlify.app/" target="_blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-500 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/cibimanoj/amazonfe.git">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-500 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
-                    Code
-                  </button>
-                  <p className="text-center text-stone-50	color: rgb(250 250 249)">
-                  This is a MERN Ecommerce App.This is a Clone of Amazon Ecommerce website, where we can Login and Register.
-                </p>
-                </a>
-                <h6 className="font-medium text-xl text-black">STACK</h6>
-                <div>
-                  <div class="flex gap-4 p-4 items-center text-lg text-zinc-50 ">
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">MongoDB </div>
-                    <div className="contents">
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500"  >Express</div>
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">React</div>
-                    </div>
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">Node</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
           <div
             style={{ backgroundImage: `url(${memories})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -103,7 +127,7 @@ const Work = () => {
                   href="https://cibimanojmemories.netlify.app/"
                   target="_blank"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-500 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-600 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
                     Demo
                   </button>
                 </a>
@@ -111,22 +135,32 @@ const Work = () => {
                   href="https://github.com/cibimanoj/memoryfe.git"
                   target="_blank"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-500 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-600 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500 ">
                     Code
                   </button>
-                  <p className="text-center text-stone-50">
-                  This is a MERN web application.In this applicaton we can save our beautiful memories of life and the data will be save in the database.
-                </p>
+                  <p className="text-center text-black	color: rgb(0 0 0)">
+                    This is a MERN web application.In this applicaton we can
+                    save our beautiful memories of life and the data will be
+                    save in the database.
+                  </p>
                 </a>
                 <h6 className="font-medium text-xl text-black">STACK</h6>
                 <div>
                   <div class="flex gap-4 p-4 items-center text-lg text-zinc-50 ">
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">MongoDB </div>
-                    <div className="contents">
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500"  >Express</div>
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">React</div>
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      MongoDB{" "}
                     </div>
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">Node</div>
+                    <div className="contents">
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        Express
+                      </div>
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        React
+                      </div>
+                    </div>
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      Node
+                    </div>
                   </div>
                 </div>
               </div>
@@ -144,7 +178,7 @@ const Work = () => {
                   href="https://socketiovideochat.netlify.app/"
                   target="_blank"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-500 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-600 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
                     Demo
                   </button>
                 </a>
@@ -152,22 +186,32 @@ const Work = () => {
                   href="https://github.com/cibimanoj/videochat_frontend.git"
                   target="_blank"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-500 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-zinc-50 bg-amber-600 font-bold text-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
                     Code
                   </button>
-                  <p className="text-center text-stone-50">
-                  This is a Videochat web application.In this app we can chat with our friends through video, And i have used socket.IO for generating the key.
-                </p>
+                  <p className="text-center text-black	color: rgb(0 0 0)">
+                    This is a Videochat web application.In this app we can chat
+                    with our friends through video, And i have used socket.IO
+                    for generating the key.
+                  </p>
                 </a>
                 <h6 className="font-medium text-xl text-black">STACK</h6>
                 <div>
                   <div class="flex gap-4 p-4 items-center text-lg text-zinc-50 ">
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">MongoDB </div>
-                    <div className="contents">
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500"  >Express</div>
-                      <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">React</div>
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      MongoDB{" "}
                     </div>
-                    <div className="flex-1 bg-amber-500 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">Node</div>
+                    <div className="contents">
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        Express
+                      </div>
+                      <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        React
+                      </div>
+                    </div>
+                    <div className="flex-1 bg-amber-600 rounded-lg shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                      Node
+                    </div>
                   </div>
                 </div>
               </div>
